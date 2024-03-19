63a687f666d5b4ccbad301e42a1b8d46f904dbd1
Grzegorz Tomaka <grzesiektomaka@gmail.com>
2024-03-19 22:21:48 +0100

    no store cors

--- medusa-config.js
+++ medusa-config.js
@@ -82,7 +82,7 @@ const modules = {
 const projectConfig = {
   jwtSecret: process.env.JWT_SECRET,
   cookieSecret: process.env.COOKIE_SECRET,
-  store_cors: STORE_CORS,
+  // store_cors: STORE_CORS,
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
