ec0e14ef83a1fda3fc193a46b2203422c34b53d1
Grzegorz Tomaka <grzesiektomaka@gmail.com>
2024-03-19 22:46:25 +0100

    cache-redis

--- medusa-config.js
+++ medusa-config.js
@@ -69,13 +69,13 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },
+  },*/
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
